28e4eabad92c30f86dbe768f0c7191d4d5efef51
nihar <nihar.shah@volgainfotech.com>
2015-01-06 17:14:43 +0530

    Add few more common function

---
+++ app/src/main/java/volga/org/pollarise/ParseJSONFromUrl.java
@@ -0,0 +1,29 @@
+package volga.org.pollarise;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+/**
+ * Created by User on 1/6/2015.
+ */
+public abstract class ParseJSONFromUrl<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+
+    protected ProgressDialog pDialog;
+    public Context context;
+
+    public ParseJSONFromUrl<Params, Progress, Result> setContext(Context c){
+        this.context = c;
+        return this;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pDialog = new ProgressDialog(this.context);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        pDialog.show();
+    }
+
+}
