192a86668308ee48cae3fdb8306327039d6142c3
nihar <nihar.shah@volgainfotech.com>
2015-01-21 11:46:31 +0530

    new change

--- app/src/main/java/volga/org/pollarise/Constants.java
+++ app/src/main/java/volga/org/pollarise/Constants.java
@@ -7,4 +7,7 @@ public class Constants {
 
     public static final int METHOD_GET = 1;
     public static final int METHOD_POST = 2;
+
+    public static final String BASE_URL = "http://www.volgapollarise.com/api/";
+
 }
